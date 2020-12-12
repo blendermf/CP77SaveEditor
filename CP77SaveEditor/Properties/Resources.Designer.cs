0b206613b76ab88636322e5810288e40bbac1da9
Matthew Fraser <1923697+blendermf@users.noreply.github.com>
2020-12-12 18:45:05 -0500

    Renamed Everything

--- CP77SaveEditor/Properties/Resources.Designer.cs
+++ CP77SaveEditor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace W3SavegameEditor.Properties {
+namespace CP77SaveEditor.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace W3SavegameEditor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("W3SavegameEditor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CP77SaveEditor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
